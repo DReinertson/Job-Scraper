8434c6d1f3db3ab1409d1bbc2b84157fb7e913a9
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2018-06-27 02:27:38 -0700

    Add files via upload
    Instead of returning each for loop. Decided to append information within the entire function. Limiting the use of for loops and preventing 5-6 different functions from existing. 
    Need to compile information next

--- Web scraper.py
+++ Web scraper.py
@@ -14,15 +14,45 @@ URL_page = requests.get(URL)
 
 ##parsing HTML into wanted format
 soup = BeautifulSoup(URL_page.text, "html.parser")
+job_list = []
+description = []
+summary = []
+location = []
+listing = {}
 
 def job_title(soup):
     jobs = []
-    ##Div class list-group-item contains title of job. Will locate list-group-item, h5 within and then the anchor tag.
+    ##Div class list-group-item contains information about jobs.
+    ##Will append respective information to correct variable and compile each variable at the end.
+
+    for desc in soup.find_all(name = "p", attrs = {"class" : "job-description"}):
+        summary.append(desc.text)
+    
     for div in soup.find_all(name = "li", attrs = {"class" : "list-group-item"}):
+
+        ##locating h5 and then anchor will give job-title
         for h in div.find_all(name = "h5"):
             for a in h.find_all(name = "a"):
-                jobs.append(a.contents[0])
-    return jobs
+                job_list.append(a.contents[0])
 
-jobs_list = job_title(soup)
-print ("jobs: ", jobs_list)
+        ##Span with class text-danger is job description
+        for span in div.find_all(name = "span", attrs = {"class" : "text-danger"}):
+            description.append(span.contents[0])
+
+        ##By using 1st index of p tag, we'll be able to find location of each job. 
+        for p in div.find_all(name = "p", attrs = {"class" : "job-type-info"}):
+            if p.contents[1] == ' ':
+                location.append("No location given")
+            else:
+                location.append(p.contents[1])
+
+        
+def contract(soup):
+    return
+    
+job_title(soup)
+print("job_list: ", len(job_list))
+print ("description: ", len(description))
+print("location: ", len(location))
+print("summary: ", len(summary))
+
